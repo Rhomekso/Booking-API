4eaf5c47a6d8648b093cdba231bd44e0f850413e
Mekso <Rhomeksoazwar@gmail.com>
2024-04-30 14:31:50 +0200

    added CRUD amenities, bookings, hosts

--- src/routes/hosts.js
+++ src/routes/hosts.js
@@ -3,9 +3,15 @@ import notFoundErrorHandler from '../middleware/notFoundErrorHandler.js';
 import NotFoundError from '../errors/NotFoundError.js';
 import auth from "../middleware/auth.js";
 import getHosts from '../services/hosts/getHosts.js';
+import getHostById from '../services/hosts/getHostById.js';
+import createHost from '../services/hosts/createHost.js';
+import updateHostById from '../services/hosts/updateHostById.js';
+import removeHostById from '../services/hosts/removeHostById.js';
 
 const router = express.Router();
 
+
+// get all hosts 
 router.get('/', async (req, res) => {
     try {
         const { name } = req.query;
@@ -18,5 +24,67 @@ router.get('/', async (req, res) => {
     }
 });
 
+// get hosts by id 
+router.get('/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const host = await getHostById(id)
+
+        res.status(200).json(host);
+    } catch (error) {
+        next(error)
+    }
+}, notFoundErrorHandler);
+
+
+// create/post host 
+router.post('/', auth, async (req, res) => {
+    try {
+        const { username, password, name, email, phoneNumber, profilePicture, aboutMe } = req.body;
+
+        if (!username || !password || !name || !email || !phoneNumber ||
+            !profilePicture || !aboutMe) {
+            return res.status(400).send('Missing required fields');
+        }
+        const newHost = await createHost(username, password, name, email, phoneNumber, profilePicture, aboutMe);
+        res.status(201).json(newHost);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Something went wrong while creating a new host!');
+    }
+});
+
+
+// update/put host by id 
+router.put('/:id', auth, async (req, res) => {
+    const { id } = req.params;
+    const { username, password, name, email, phoneNumber, profilePicture, aboutMe } = req.body;
+
+    try {
+        const updatedHost = await updateHostById(id, username, password, name, email, phoneNumber, profilePicture, aboutMe);
+        res.status(200).json(updatedHost);
+    } catch (error) {
+        if (error instanceof NotFoundError) {
+            res.status(404).json({ error: error.message });
+        } else {
+            console.error('Error in updateHostById:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+});
+
+// remove/delete host
+router.delete('/:id', auth, async (req, res) => {
+    const { id } = req.params;
+    const host = await removeHostById(id);
+
+    if (host !== null) {
+        res.status(200).json(host);
+    } else {
+        res.status(404).json({
+            error: `Host with id ${id} not found.`,
+        });
+    }
+});
 
 export default router;
