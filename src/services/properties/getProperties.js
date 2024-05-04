f9eb59cc4f697bdf5d7c64791d034beec56591e8
Mekso <Rhomeksoazwar@gmail.com>
2024-05-04 17:08:03 +0200

    added comments

--- src/services/properties/getProperties.js
+++ src/services/properties/getProperties.js
@@ -11,7 +11,7 @@ const getProperties = async (id) => {
 			}
         }
     });
-	console.log("foundWithPrisma:",foundWithPrisma)
+	// console.log("foundWithPrisma:",foundWithPrisma)
 	return foundWithPrisma
 }
 
